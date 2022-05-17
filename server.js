e690385f7c99329d84a4baac81530e7a6b7be907
charles vhutuzah <charles.vhutuza@riskflow.com>
2022-05-17 13:24:29 +0200

    project structure

---
+++ server.js
@@ -0,0 +1,18 @@
+//Define Routes
+//Create a Controller
+// Create objects
+// retrieve objects
+// retrieve a single object
+// update an object
+// delete an object
+// delete all objects
+// test the API
+//  Methods    Urls                Actions
+//  GET        api/tutorials       get All tutorials
+//  GET        api/tutorials/:id   get Tutorial by id
+//  POST       api/tutorials       add new tutorial
+//  PUT        api/tutorials/:id   update Tutorial by id
+//  DELETE     api/tutorials/:id   remove Tutorial by id
+//  DELETE     api/tutorials       remove all Tutorials
+
+//  Test the API using Postman
