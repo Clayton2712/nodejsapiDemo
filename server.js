c426085337936b9b68490fc3e018ac2aa1e5311b
charles vhutuzah <charles.vhutuza@riskflow.com>
2022-05-17 14:08:37 +0200

    server

--- server.js
+++ server.js
@@ -16,3 +16,25 @@
 //  DELETE     api/tutorials       remove all Tutorials
 
 //  Test the API using Postman
+
+// File names
+// app/config/db.config.js
+// app/controllers/tutorial.controller.js
+// app/models/db.js
+// app/models/tutorial.model.js
+// app/routes/tutorial.routes.js
+// 1. use the npm to install mysql
+// 2. use the npm to install cors
+//
+var express = require('express');
+var cors = require('cors');
+var  app = express();
+
+// CORS stands for Cross-Origin Resource Sharing. It allows us to relax the security
+// applied to an API
+var corsOptions = {
+    origin: "http://localhost:8081"
+}
+console.log('Is our project running?');
+
+
